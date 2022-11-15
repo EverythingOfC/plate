4131baea028d70c74723c80685ba7511417caac9
서성준 <tkflwk123@gmail.com>
2022-11-15 13:51:07 +0900

    sj commit

--- plate/admin.py
+++ plate/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Admin, Car  # models.py에서 모델들을 가져옴.
+
+admin.site.register(Admin)  # Admin 클래스를 admin에 등록
+admin.site.register(Car)  # Car 클래스를 admin에 등록
