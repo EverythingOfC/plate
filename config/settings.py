15d45a24712ceda77263851d1035e7bba9bd3d26
서성준 <tkflwk123@gmail.com>
2022-11-17 12:25:00 +0900

    연습용

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'plate.apps.PlateConfig',
+    'django_cleanup.apps.CleanupConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
