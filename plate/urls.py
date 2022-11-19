d8188a4b18f169616961b2e919e73075ff1596df
서성준 <tkflwk123@gmail.com>
2022-11-19 11:21:35 +0900

    최종 본 ( 로그인 기능 추후 개선 )

--- plate/urls.py
+++ plate/urls.py
@@ -7,8 +7,8 @@ app_name = 'plate'
 
 urlpatterns = [
     path('',views.index, name='index'),  # 기본 메인
-    path('det/<int:car_id>',views.det_index, name='det_index'),  # 검출 후 메인
+    path('det',views.det_index, name='det_index'),  # 검출 후 메인
     path('create/', views.plate_create, name='plate_create'),  # 등록
-    path('withhold/<int:car_id>/<str:a_id>', views.plate_withhold,name='plate_withhold'),  # 보류
+    path('withhold/<int:c_id>/<str:a_id>', views.plate_withhold,name='plate_withhold'),  # 보류
     path('start/<str:a_id>',views.plate_start,name='start'),  # 검출
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- plate/views.py
+++ plate/views.py
@@ -3,8 +3,10 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404,redirect
 from .forms import CarForm
 from .car_exe import classify_number
+from django.utils import timezone
 
 # Create your views here.
+
 
 def index(request): # 기본 페이지
   page = request.GET.get('page', '1')  # 페이지
@@ -20,7 +22,7 @@ def index(request): # 기본 페이지
   page_obj2 = paginator2.get_page(page)
   page_obj3 = paginator3.get_page(page)
 
-  context = {'citizen_list': page_obj, 'admin': page_obj2, 'car_list':page_obj3 }  # 차량과 시민정보를 index에 전달
+  context = {'citizen_list': page_obj, 'admin': page_obj2, 'car_list': page_obj3 }  # 차량과 시민정보를 index에 전달
   return render(request, 'plate/index.html', context)
 
 
@@ -31,9 +33,19 @@ def plate_create(request):  # 차 등록
   return render(request, 'plate/form.html', context)  # 폼 페이지
 
 
-def plate_withhold(request, car_id, a_id):  # 차 보류
-  car = get_object_or_404(Car, pk=car_id)
+def plate_withhold(request, c_id, a_id):  # 차 보류
+  car = get_object_or_404(Car, pk=c_id)
   admin = get_object_or_404(Admin, pk=a_id)
+  citizen_list = Citizen.objects.order_by('c_num')
+
+  citizen = None  #  시민 객체 삭제를 위한 변수
+
+  for cit in citizen_list:
+    if cit.c_num == car.car_num:
+        citizen = get_object_or_404(Citizen, pk=cit.id)
+        citizen.delete()  # 시민 삭제
+        break
+
   admin.a_wh += 1  # 보류 횟수 1증가
   admin.a_id = a_id
   car.delete()
@@ -42,8 +54,17 @@ def plate_withhold(request, car_id, a_id):  # 차 보류
 
 
 def plate_start(request,a_id):  # 번호판 검출 ( 저장하기 누르면 )
+  citizen_list = Citizen.objects.order_by('c_num')  # 시민 정보를 가져옴.
   car = Car()  # Car모델 객체 생성
   car.car_num = request.POST['car_num']
+
+  citizen = None  # 시민 객체 (단속횟수 저장을 위함)
+
+  for cit in citizen_list:
+    if cit.c_num == car.car_num:  # 해당 시민의 번호판과 일치하면
+      citizen = get_object_or_404(Citizen,pk=cit.id)
+      break
+
   car.car_image = request.FILES['car_image']
   car_no = request.FILES['car_image']
   print(car_no)
@@ -51,23 +72,27 @@ def plate_start(request,a_id):  # 번호판 검출 ( 저장하기 누르면 )
 
   admin = get_object_or_404(Admin,pk=a_id)
   if classify_number.start(car_no)[1] != -1:
-    admin.a_de += 1  # 검출 횟수 1증가
+    admin.a_de += 1  # 검출 횟수 1 증가
     car.car_check = 1  # 검출 차량으로 분류
+    citizen.c_count += 1  # 단속 횟수
+    citizen.c_date = timezone.now()  # 단속 일자
+    admin.save()
+    citizen.save()
 
   if car.car_num == request.POST['car_num']:
     car.car_image = "images/r_%s"%car_no
 
   car.save()
-  return redirect('plate:det_index',car_num = car.car_num)
+  return redirect('plate:det_index')
 
 
-def det_index(request, car_num):  # 검출 후 기본 페이지
+def det_index(request):  # 검출 후 기본 페이지
   page = request.GET.get('page', '1')  # 페이지
-  citizen_list = Citizen.objects.order_by('c_num')
+  citizen_list = Citizen.objects.order_by('c_num')  #  번호로 정렬
   car_list = Car.objects.order_by('car_num')
   admin = Admin.objects.filter(a_id='tkflwk23')
 
-  paginator = Paginator(citizen_list, 1)   # 페이지당 1개씩 보여주기
+  paginator = Paginator(citizen_list, 1)  # 페이지 당 1개씩 보여주기
   paginator2 = Paginator(admin, 1)
   paginator3 = Paginator(car_list, 1)
 
