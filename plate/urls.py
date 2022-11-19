1825d517b28318c11597a4a0a51a6e0e14057e91
서성준 <tkflwk123@gmail.com>
2022-11-19 18:00:44 +0900

    최종 본 ( 로그인 기능 추후 개선 )

--- plate/urls.py
+++ plate/urls.py
@@ -6,8 +6,9 @@ from plate import views
 app_name = 'plate'
 
 urlpatterns = [
-    path('',views.index, name='index'),  # 기본 메인
-    path('det',views.det_index, name='det_index'),  # 검출 후 메인
+    path('login/', views.login, name='login'),  # 로그인
+    path('<str:a_id>',views.index, name='index'),  # 기본 메인
+    path('det/<str:a_id>/',views.det_index, name='det_index'),  # 검출 후 메인
     path('create/', views.plate_create, name='plate_create'),  # 등록
     path('withhold/<int:c_id>/<str:a_id>', views.plate_withhold,name='plate_withhold'),  # 보류
     path('start/<str:a_id>',views.plate_start,name='start'),  # 검출
--- plate/views.py
+++ plate/views.py
@@ -1,28 +1,41 @@
 from .models import Car,Admin,Citizen
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404,redirect
-from .forms import CarForm
+from .forms import CarForm,AdminForm
 from .car_exe import classify_number
 from django.utils import timezone
 
-# Create your views here.
+
+def login(request):  # 로그인 페이지
+  if request.method == "POST":
+    id = request.POST['id']
+    pw = request.POST['password']
+    admin = Admin.objects.order_by('a_id')  # 관리자 데이터를 받아옴.
+
+    for a in admin:
+      if a.a_id == id and a.a_pw == pw:  # 하나라도 일치하면 로그인 성공
+        return redirect('plate:index', a_id=a.a_id)  # 해당 관리자를 넘김
+
+  form = AdminForm()
+  context = {'form': form }
+  return render(request, 'plate/login.html', context)  # 폼 페이지
 
 
-def index(request): # 기본 페이지
+def index(request,a_id): # 기본 페이지
   page = request.GET.get('page', '1')  # 페이지
   citizen_list = Citizen.objects.order_by('c_num')  # 시민 리스트
   car_list = Car.objects.order_by('car_num')  # 차량 리스트
-  admin = Admin.objects.filter(a_id='tkflwk23')
+  admin = Admin.objects.filter(a_id=a_id)  # 해당 관리자 로그인
 
-  paginator = Paginator(citizen_list,1)  # 페이지당 1개씩 보여주기
+  # paginator = Paginator(citizen_list,1)  # 페이지당 1개씩 보여주기
   paginator2 = Paginator(admin,1)
   paginator3 = Paginator(car_list,1)
 
-  page_obj = paginator.get_page(page)  # 페이지 객체
+  # page_obj = paginator.get_page(page)  # 페이지 객체
   page_obj2 = paginator2.get_page(page)
   page_obj3 = paginator3.get_page(page)
 
-  context = {'citizen_list': page_obj, 'admin': page_obj2, 'car_list': page_obj3 }  # 차량과 시민정보를 index에 전달
+  context = {'citizen_list': citizen_list, 'admin': page_obj2, 'car_list': page_obj3 }  # 차량과 시민정보를 index에 전달
   return render(request, 'plate/index.html', context)
 
 
@@ -36,21 +49,12 @@ def plate_create(request):  # 차 등록
 def plate_withhold(request, c_id, a_id):  # 차 보류
   car = get_object_or_404(Car, pk=c_id)
   admin = get_object_or_404(Admin, pk=a_id)
-  citizen_list = Citizen.objects.order_by('c_num')
-
-  citizen = None  #  시민 객체 삭제를 위한 변수
-
-  for cit in citizen_list:
-    if cit.c_num == car.car_num:
-        citizen = get_object_or_404(Citizen, pk=cit.id)
-        citizen.delete()  # 시민 삭제
-        break
 
   admin.a_wh += 1  # 보류 횟수 1증가
   admin.a_id = a_id
   car.delete()
   admin.save()
-  return redirect('plate:index')  # 삭제 후, 메인 페이지로 돌아감.
+  return redirect('plate:index',a_id=a_id)  # 삭제 후, 메인 페이지로 돌아감.
 
 
 def plate_start(request,a_id):  # 번호판 검출 ( 저장하기 누르면 )
@@ -83,22 +87,21 @@ def plate_start(request,a_id):  # 번호판 검출 ( 저장하기 누르면 )
     car.car_image = "images/r_%s"%car_no
 
   car.save()
-  return redirect('plate:det_index')
+  return redirect('plate:det_index',a_id=a_id)
 
 
-def det_index(request):  # 검출 후 기본 페이지
+def det_index(request,a_id):  # 검출 후 기본 페이지
   page = request.GET.get('page', '1')  # 페이지
   citizen_list = Citizen.objects.order_by('c_num')  #  번호로 정렬
   car_list = Car.objects.order_by('car_num')
-  admin = Admin.objects.filter(a_id='tkflwk23')
+  admin = Admin.objects.filter(a_id=a_id)
 
-  paginator = Paginator(citizen_list, 1)  # 페이지 당 1개씩 보여주기
   paginator2 = Paginator(admin, 1)
   paginator3 = Paginator(car_list, 1)
 
-  page_obj = paginator.get_page(page)  # 페이지 객체
+  # page_obj = paginator.get_page(page)  # 페이지 객체
   page_obj2 = paginator2.get_page(page)
   page_obj3 = paginator3.get_page(page)
 
-  context = {'citizen_list': page_obj, 'admin': page_obj2, 'car_list': page_obj3}  # 차량과 시민정보를 index에 전달
+  context = {'citizen_list': citizen_list, 'admin': page_obj2, 'car_list': page_obj3}  # 차량과 시민정보를 index에 전달
   return render(request, 'plate/index.html', context)
